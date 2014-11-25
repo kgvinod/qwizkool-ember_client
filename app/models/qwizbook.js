5ac6c046696a98b4ff2b683c1974a85d27d2fec9
Balagopal Ambalakkat <bambalakkat@gmail.com>
2014-11-25 10:17:56 -0800

    added REST adapter and qwizbooks route

--- app/models/qwizbook.js
+++ app/models/qwizbook.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
 
+//var attr = DS.attr;
+
 export default DS.Model.extend({
-  
+  title:  DS.attr('string'),
+  subtitle:  DS.attr('string'),
+  owner:  DS.attr('string'),
+  description:  DS.attr('string'),
+  createdOn:  DS.attr('string'),
+  public:  DS.attr('boolean'),
+  sharingEnabled:  DS.attr('boolean'),
+
 });
