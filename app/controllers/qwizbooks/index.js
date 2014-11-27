8d78fef760212aacbd0acdff92f46454c50fda37
Balagopal Ambalakkat <bambalakkat@gmail.com>
2014-11-27 12:07:52 -0800

    Added support for query params in index controller. Added support to refresh model for the route,if query changed

--- app/controllers/qwizbooks/index.js
+++ app/controllers/qwizbooks/index.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 
 export default Ember.ArrayController.extend({
+  queryParams: ['search'],
+  search: null,
+  actions: {
+
+    clearSearch: function () {
+      this.set('search',null);
+      console.log("Hello");
+    }
+
+
+  }
 
 });
--- app/routes/qwizbooks/index.js
+++ app/routes/qwizbooks/index.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('qwizbook');
+  queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+  model: function(params) {
+    return this.store.find('qwizbook',params);
   }
 });
