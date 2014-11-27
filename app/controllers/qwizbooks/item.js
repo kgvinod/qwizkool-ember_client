46febf7fec121be9c0b95ed0c0118e5ece286a0e
Balagopal Ambalakkat <bambalakkat@gmail.com>
2014-11-27 08:01:22 -0800

    added support for qwizbook index controller and item controller. added a property in item controller and did a conditional in the template based on this property

---
+++ app/controllers/qwizbooks/item.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+
+  // Need to set this to true if the qwizbook
+  // has an Image URL for the book.
+  hasImageUrl:false
+});
