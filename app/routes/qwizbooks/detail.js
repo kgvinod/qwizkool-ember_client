7ac8d0e2ce476d21dbcf9e8ff9035f946b9609f2
Balagopal Ambalakkat <bambalakkat@gmail.com>
2014-12-10 12:06:46 -0800

    provide the model for detail using id instead of default. this allows page to be consistent when refreshed

--- app/routes/qwizbooks/detail.js
+++ app/routes/qwizbooks/detail.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+      return this.store.find('qwizbook',params.qwizbook_id);
+  },
   actions: {
 
     startQwiz: function () {
