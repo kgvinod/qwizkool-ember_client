802ad991b90499316c0c5a68c00daa289d210d5f
Balagopal Ambalakkat <bambalakkat@gmail.com>
2014-12-14 18:03:21 -0800

    consistent url scheme

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,4 +3,11 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
     url: 'http://localhost:3000',
     namespace: 'api',
+    buildURL: function() {
+      var url = this._super.apply(this, arguments);
+      if (url.charAt(url.length -1) !== '/') {
+        url += '/';
+      }
+      return url;
+    },
 });
