52390c0ccd037dfe9618dc540039ebaa21b6a177
Balagopal Ambalakkat <bambalakkat@gmail.com>
2015-01-31 19:49:40 -0800

    sample embed url for audio and video

--- app/models/media-url.js
+++ app/models/media-url.js
@@ -7,3 +7,11 @@ export default DS.Model.extend({
   mediaUrl: DS.attr('string',{ defaultValue: "http://bala.qwizkool.com/assets/images/question-image.png" }),
   wideMedia:     DS.attr('boolean',{ defaultValue: false }),
 });
+
+// All urls need to be emded type
+// Soundcloud url
+// https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/165018741&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true
+// Vimeo url
+// //player.vimeo.com/video/118046581?title=0&byline=0&portrait=0
+// Youtube url
+// https://www.youtube.com/embed/zpOULjyy-n8
