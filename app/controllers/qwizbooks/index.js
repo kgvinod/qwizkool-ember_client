5eb6dc38b95a04b3a662fb7b53de5b1c56ed4f71
Vinod Gopalan <kgvinod@gmail.com>
2016-06-04 20:58:22 -0700

    reverted the search hack after fixing properly at backend

--- app/controllers/qwizbooks/index.js
+++ app/controllers/qwizbooks/index.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   queryParams: ['search'],
-  search: 'o',  // HACK ! to ensure that it doesn't send empty search=
+  search: null,  
   actions: {
 
     clearSearch: function () {
-      this.set('search','o'); // HACK ! to ensure that it doesn't send empty search=
+      this.set('search', null); 
     },
 
   }
